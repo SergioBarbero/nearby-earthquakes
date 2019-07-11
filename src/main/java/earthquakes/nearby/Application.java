7e94bdddedf88c45ec50629539f3c7fd87206053
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-07-11 09:32:24 +0200

    Sorting by comparator works

--- src/main/java/earthquakes/nearby/Application.java
+++ src/main/java/earthquakes/nearby/Application.java
@@ -3,21 +3,26 @@ package earthquakes.nearby;
 import com.fasterxml.jackson.core.JsonFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.source.tree.Tree;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Application {
 
     static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String args[]) {
+        Coordinates reference = new Coordinates(40.730610, -73.935242);
         String url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
         JsonFactory jsonFactory = new JsonFactory();
         Earthquakes information = null;
         try {
-            Earthquakes earthquakes = new ObjectMapper().readValue(new URL(url), Earthquakes.class);
-            System.out.println("");
+            Set<Feature> features = new ObjectMapper().readValue(new URL(url), Earthquakes.class).getFeatures();
+            TreeSet<Feature> top = new TreeSet<Feature>(new SortByProximity(reference));
+            top.addAll(features);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/earthquakes/nearby/SortByProximity.java
+++ src/main/java/earthquakes/nearby/SortByProximity.java
@@ -1,6 +1,10 @@
 package earthquakes.nearby;
 
 import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 public class SortByProximity implements Comparator<Feature> {
 
@@ -12,8 +16,13 @@ public class SortByProximity implements Comparator<Feature> {
 
     @Override
     public int compare(Feature o1, Feature o2) {
-        double distance1 = reference.calculateDistanceTo(o1.getCoordinates());
-        double distance2 = reference.calculateDistanceTo(o2.getCoordinates());
+        double distance1 = Math.abs(reference.calculateDistanceTo(o1.getCoordinates()));
+        double distance2 = Math.abs(reference.calculateDistanceTo(o2.getCoordinates()));
         return Double.compare(distance1, distance2);
+    }
+
+    @Override
+    public Comparator<Feature> reversed() {
+        return null;
     }
 }
